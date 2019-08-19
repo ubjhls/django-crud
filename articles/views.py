b3977d352db40a4e811b4dd7b11f941d99c5529c
ubjhls <ubjhls>
2019-08-19 16:40:08 +0900

    04 | articles - CRUD - C

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Article
 # Create your views here.
 def index(request):
-    articles = Article.objects.all()
+    articles = Article.objects.order_by('-id')
 
     context = {
         'articles' : articles
     }
     return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request,'articles/new.html')
+
+def create(request):
+    # 저장 로직
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article(title=title, content=content)
+    context = {
+        'article' : article
+    }
+    # return render(request, 'articles/create.html', context)
+    return redirect('/articles/')
